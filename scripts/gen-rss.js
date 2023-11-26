9027938b969e8b2bdffc231254ac6da6b8642397
Utsav Oza <utsavoza96@gmail.com>
2023-11-25 19:08:36 -0500

    About time

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml',
+    title: 'Utsav Oza',
+    site_url: 'https://utsavoza.github.io',
+    feed_url: 'https://utsavoza.github.io/feed.xml',
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   footer: (
     <footer>
       <small>
-        <time>{YEAR}</time> © Your Name.
+        <time>{YEAR}</time> © Utsav Oza.
         <a href="/feed.xml">RSS</a>
       </small>
       <style jsx>{`
